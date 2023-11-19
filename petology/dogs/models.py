a647152d436463617eaeb28dbb74d14bc96929fe
gustafgroning <gustaf.groning@sigicom.com>
2023-11-19 17:10:32 +0100

    worked on auth, finished except for JSON web tokens

--- petology/dogs/models.py
+++ petology/dogs/models.py
@@ -4,7 +4,16 @@ from django.db import models
 # impacts how many of which type of questions users will receive daily.
 class Breed(models.Model):
     name = models.CharField(max_length=100)
+    # Add INT fields for handling health index stats. Will be used 
+    # for randomizing daily questions.
 
+    # Health index areas: 
+        # Allmäntillstånd 
+        # övrigt 
+        # munhälsa 
+        # ögon 
+        # hud och päls 
+        # rörelseapparaten
 
 
 class Dog(models.Model):
--- petology/users/views.py
+++ petology/users/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+        
+        if not username or not password or not email:
+            return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    # This view requires the user to be authenticated
+    return Response({'message': 'You have access to the protected view'}, status=status.HTTP_200_OK)
