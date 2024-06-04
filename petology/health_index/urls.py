9b20237a2d13377fa48fc7388ade832f20a43575
gustafgroning <gustaf.groning@sigicom.com>
2024-06-04 13:41:17 +0200

    finished mvp of health index

--- petology/health_index/urls.py
+++ petology/health_index/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import get_all_rows_for_dog, get_latest_row_for_dog, get_questions_in_batch
+from .views import get_all_rows_for_dog, get_latest_row_for_dog, get_questions_in_batch, save_new_health_index_row
 
 urlpatterns = [
    # DogHealthIndex
    path('dog/get/all/<int:dog_id>/', get_all_rows_for_dog, name='get_all_rows_for_dog'),
    path('dog/get/<int:dog_id>/', get_latest_row_for_dog, name="get_latest_row_for_dog"),
    path('batch/get/<int:batch_id>/', get_questions_in_batch, name='get_questions_in_batch'),
+   path('dog/<int:dog_id>/add/', save_new_health_index_row, name='save_new_health_index_row'),
 
 ]
--- petology/health_index/views.py
+++ petology/health_index/views.py
@@ -15,7 +15,7 @@ from rest_framework import status
 from .serializers import DogHealthIndexSerializer, HealthIndexQuestionSerializer
 
 # from .models import Dog, Breed
-from .models import HealthIndexBatch, DogHealthIndex, HealthIndexQuestion
+from .models import HealthIndexBatch, DogHealthIndex, HealthIndexQuestion, Dog
 # from ..dogs.models import Dog, Breed
 
 
@@ -49,6 +49,44 @@ def get_all_rows_for_dog(request, dog_id):
         serializer = DogHealthIndexSerializer(dog_rows, many=True)
         return Response(serializer.data)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def save_new_health_index_row(request, dog_id):
+    try:
+        # Fetch the dog object
+        dog = get_object_or_404(Dog, id=dog_id)
+
+        # Extract data from the request
+        data = request.data
+        latest_run_batch_id = data.get('latest_run_batch_id')
+        batches_in_row = data.get('batches_in_row')
+        date_performed = data.get('date_performed')
+        general_condition = data.get('general_condition')
+        dental_health = data.get('dental_health')
+        eyes = data.get('eyes')
+        skin_and_coat = data.get('skin_and_coat')
+        locomotor_system = data.get('locomotor_system')
+        other = data.get('other')
+
+        # Create a new DogHealthIndex row
+        new_row = DogHealthIndex.objects.create(
+            dog=dog,
+            latest_run_batch_id=latest_run_batch_id,
+            batches_in_row=batches_in_row,
+            date_performed=date_performed,
+            general_condition=general_condition,
+            dental_health=dental_health,
+            eyes=eyes,
+            skin_and_coat=skin_and_coat,
+            locomotor_system=locomotor_system,
+            other=other,
+        )
+
+        # Serialize the new row
+        serializer = DogHealthIndexSerializer(new_row)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 """
 POST
 # New values might be a part of the request?
